f5acddb5569cefaf3c96e0246c89a9f544d948a9
Bruce Becker <brucellino@gmail.com>
2017-08-11 13:29:59 +0200

    ignore cert

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ mkdir -p ${SOFT_DIR}
 if [ ! -e ${SRC_DIR}/${SOURCE_FILE}.lock ] && [ ! -s ${SRC_DIR}/${SOURCE_FILE} ] ; then
   touch  ${SRC_DIR}/${SOURCE_FILE}.lock
   echo "seems like this is the first build - let's geet the source"
-  wget http://www.hepforge.org/archive/lhapdf/${SOURCE_FILE} -O ${SRC_DIR}/${SOURCE_FILE}
+  wget --no-check-certificate http://www.hepforge.org/archive/lhapdf/${SOURCE_FILE} -O ${SRC_DIR}/${SOURCE_FILE}
   echo "releasing lock"
   rm -v ${SRC_DIR}/${SOURCE_FILE}.lock
 elif [ -e ${SRC_DIR}/${SOURCE_FILE}.lock ] ; then
